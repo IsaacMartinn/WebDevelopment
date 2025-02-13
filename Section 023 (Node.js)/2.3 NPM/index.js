1fb8a8db249191fda111ca431e249f90e190c737
Isaac Martin <isaacisamartin@gmail.com>
2025-02-13 11:36:10 -0500

    Added Section 023 (Node.js)

---
+++ Section 023 (Node.js)/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+
+// console.log(`My name is ${sillyName}`); //fstring use backstring top left
+
+
+
+import superheros, {randomSuperhero} from 'superheroes';
+var name = randomSuperhero();
+console.log(`My name is ${name}!`);
